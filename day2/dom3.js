9f4b949b0fc5eaa7ce92db7cffa7b60bb3fb8e4b
Prikshit Sharma <153969074+Prikshithp@users.noreply.github.com>
2024-01-02 13:08:12 +0530

    Create dom3.js

---
+++ day2/dom3.js
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chai or Code</title>
+</head>
+<body>
+   <!-- create a div element -->
+   
+   <script>
+    document.createElement('div')
+    div.id='myid';
+    div.className='myClass'
+    // wanna add some more attribue then
+    div.setAttribute('title','generated title')
+
+    // add text to div
+    const addtext=document.createTextNode("chai or code")
+    div.append(addtext);
+    document.body.appendChild(div)
+   </script>
+
+const 
+</body>
+</html>
